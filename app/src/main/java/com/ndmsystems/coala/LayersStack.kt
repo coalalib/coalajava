6eba2d2544370d568ffbd3defb5f3132d5e1abaa
Bas9312 <Bas9312@yandex.ru>
2022-04-28 17:54:21 +0500

    KeeneticOS: add loader with progress https://app.asana.com/0/1202017088075314/1202173531787238 https://app.asana.com/0/1202017088075314/1202039954381641

--- app/src/main/java/com/ndmsystems/coala/LayersStack.kt
+++ app/src/main/java/com/ndmsystems/coala/LayersStack.kt
@@ -1,47 +1,47 @@
-package com.ndmsystems.coala;
+package com.ndmsystems.coala
 
-import com.ndmsystems.coala.layers.ReceiveLayer;
-import com.ndmsystems.coala.layers.SendLayer;
-import com.ndmsystems.coala.message.CoAPMessage;
-import com.ndmsystems.coala.utils.Reference;
-
-import java.net.InetSocketAddress;
+import com.ndmsystems.coala.layers.ReceiveLayer
+import com.ndmsystems.coala.layers.SendLayer
+import com.ndmsystems.coala.layers.arq.ArqLayer
+import com.ndmsystems.coala.layers.arq.states.LoggableState
+import com.ndmsystems.coala.message.CoAPMessage
+import com.ndmsystems.coala.utils.Reference
+import java.net.InetSocketAddress
 
 //TODO стоит разделить класс на два - ReceiveLayerStack и SendLayerStack т.к. их функции не пересекаются (см. следующую тудушку)
-public class LayersStack {
-
-    private SendLayer[] sendStack;
-    private ReceiveLayer[] receiveStack;
-
-    public class InterruptedException extends Exception {
-    }
-
-    public LayersStack(SendLayer[] sendStack,
-                       ReceiveLayer[] receiveStack){
-        this.sendStack = sendStack;
-        this.receiveStack = receiveStack;
-    }
+class LayersStack(
+    private val sendStack: Array<SendLayer>?,
+    private val receiveStack: Array<ReceiveLayer>?
+) {
+    inner class InterruptedException : Exception()
 
     //TODO методы onReceive и onSend никак не связаны, зачем они находятся в одном классе? Основной критерий объединения методов в классы - их сильная связность. Но здесь связность нулевая.
-    public void onReceive(CoAPMessage message, Reference<InetSocketAddress> senderAddressReference) throws LayersStack.InterruptedException {
-        for (ReceiveLayer layer : receiveStack) {
-            boolean shouldContinue = layer.onReceive(message, senderAddressReference);
-
-            if (!shouldContinue) {
-                break;
+    @Throws(InterruptedException::class)
+    fun onReceive(message: CoAPMessage, senderAddressReference: Reference<InetSocketAddress?>?) {
+        receiveStack?.let {
+            for (layer in receiveStack) {
+                val shouldContinue = layer.onReceive(message, senderAddressReference)
+                if (!shouldContinue) {
+                    break
+                }
             }
         }
     }
 
-    public boolean onSend(CoAPMessage message, Reference<InetSocketAddress> receiverAddressReference) throws LayersStack.InterruptedException {
-        for (SendLayer layer : sendStack) {
-            boolean shouldContinue = layer.onSend(message, receiverAddressReference);
-
-            if (!shouldContinue) {
-                return false;
+    @Throws(InterruptedException::class)
+    fun onSend(message: CoAPMessage, receiverAddressReference: Reference<InetSocketAddress?>?): Boolean {
+        sendStack?.let {
+            for (layer in sendStack) {
+                val shouldContinue = layer.onSend(message, receiverAddressReference)
+                if (!shouldContinue) {
+                    return false
+                }
             }
         }
+        return true
+    }
 
-        return true;
+    fun getArqReceivedStateForToken(token: ByteArray): LoggableState? {
+        return (receiveStack?.find { it is ArqLayer } as ArqLayer?)?.getArqReceivingStateForToken(token)
     }
 }
